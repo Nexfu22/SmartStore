7c4009a6d0db7877ee2ee558f8c6a20881bfd4da
chrisbenoi <162717533+chrisbenoi@users.noreply.github.com>
2026-05-04 20:36:26 +0300

    Update templates.h

--- templates.h
+++ templates.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
+template <typename T>
+T findHighestPrice(T arr[], int size) {
+    T highest = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i]->getPrice() > highest->getPrice()) {
+            highest = arr[i];
+        }
+    }
+    return highest;
+}
+
+template <typename T>
+void swapProducts(T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
