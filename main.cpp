212cc7adad923754fa80bb5640c0706698a0bdb6
Nexfu22 <ym2222007m@gmail.com>
2026-05-04 13:51:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,18 +5,14 @@
 
 
 int main() {
-    cout << "==============================" << endl;
-    cout << "   Smart Store - Student 1    " << endl;
-    cout << "==============================" << endl;
-
-    // Create 4 products using base class pointers (polymorphism)
+    // Create 4 products
     Product* products[4];
     products[0] = new Book(101, "Clean Code", 250.0, 10, "Robert Martin", 431);
     products[1] = new ElectronicDevice(102, "Samsung Galaxy", 15000.0, 5, "Samsung", 24);
     products[2] = new OfficeSupply(103, "Stapler", 75.0, 30, "Fasteners", "Metal");
     products[3] = new Book(104, "Harry Potter", 180.0, 8, "J.K. Rowling", 500);
 
-    // Display all products (calls overridden displayInfo)
+    // Display all products
     cout << "\n===== All Products =====" << endl;
     for (int i = 0; i < 4; i++) {
         products[i]->displayInfo();
@@ -29,7 +25,7 @@ int main() {
     cout << "\n===== Purchase Operations =====" << endl;
     products[0]->buyOne();
     products[1]->buyMultiple(3);
-    products[2]->buyMultiple(100); // not enough stock
+    products[2]->buyMultiple(100); 
 
     // File I/O
     cout << "\n===== File Operations =====" << endl;
